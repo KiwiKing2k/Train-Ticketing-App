ace3c9c4ce280bcb6c4b72dbfd853da613461a6f
Fireballer <dinustefan04@gmail.com>
2024-11-30 20:19:42 +0200

    Header for Utils

---
+++ Utils/Utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+bool is_valid_date_format(const std::string& date);
+bool is_valid_year(const std::string& date);
+bool is_valid_city_name(const std::string& cityName);
+
+#endif // UTILS_H
