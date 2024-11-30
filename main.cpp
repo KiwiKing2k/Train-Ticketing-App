c510870f0ea7abfd26c0bce32fcb705fc4438d09
Fireballer <dinustefan04@gmail.com>
2024-11-30 17:49:01 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef struct Wagon
+{
+    int number;
+    int max_capacity=80; /// standard for CFR
+
+};
+
+class TrainRide
+{
+private:
+    string name;
+    char date[8];
+    char time[5];
+    string destination;
+    string origin;
+
+
+};
