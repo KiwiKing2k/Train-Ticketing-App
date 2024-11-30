73601ba62aca81995901907e94afbce58e167b72
Fireballer <dinustefan04@gmail.com>
2024-11-30 20:22:53 +0200

    Main used for testing of different functions. Users and authentication yet to be handled.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    TrainRide train("Train1", "12.12.2020", "12:00", "Bucharest", "Cluj", 2, 3);
+    TrainRide train("Train1", "12.12.2025", "12:00", "Bucharest", "Cluj", 2, 3);
     cout << train.reserve_any_seat(1) << endl;
     return 0;
 }
