545e38d5873a60c0a81e1bfed9ae23de0bc1c5ed
Fireballer <dinustefan04@gmail.com>
2024-11-30 20:21:45 +0200

    The Class responsible for the functions of a train and its ticketing, now adapted for checking for correct information

--- TrainRide.cpp
+++ TrainRide.cpp
@@ -1,9 +1,33 @@
 #include "TrainRide.h"
+#include "Utils/Utils.h"
+#include <regex>
 
 TrainRide::TrainRide(string name, string date, string time, string destination, string origin, int first_class_wagons, int second_class_wagons)
     : name(name), date(date), time(time), destination(destination), origin(origin),
       wagons(new Wagon[first_class_wagons + second_class_wagons]), first_class(first_class_wagons), second_class(second_class_wagons)
 {
+    //date format check
+    if (is_valid_date_format(date) == false)
+    {
+        throw std::invalid_argument("Invalid date format.");
+    }
+
+    //time check
+    if (is_valid_year(date) == false)
+    {
+        throw std::invalid_argument("This program is designed for usage after 01.01.2025");
+    }
+
+    //City name check
+    if (is_valid_city_name(destination) == false )
+    {
+        throw std::invalid_argument("Invalid destination city name.");
+    }
+    if (is_valid_city_name(origin) == false)
+    {
+        throw std::invalid_argument("Invalid origin city name.");
+    }
+
     for (int i = 0; i < first_class_wagons; i++)
     {
         this->wagons[i].WagonAssigner(1);
