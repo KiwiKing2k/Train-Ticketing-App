c0bbfc80ae91e5551f4edd7d7ec06c23cefe8795
Fireballer <dinustefan04@gmail.com>
2024-11-30 19:04:45 +0200

    Header file for TrainRide

---
+++ TrainRide.h
@@ -0,0 +1,27 @@
+#ifndef TRAINRIDE_H
+#define TRAINRIDE_H
+
+#include <iostream>
+#include <string>
+#include "Wagon.h"
+using namespace std;
+
+class TrainRide
+{
+private:
+    string name;
+    string date;
+    string time;
+    string destination;
+    string origin;
+    Wagon* wagons;
+    int first_class;
+    int second_class;
+
+public:
+    TrainRide(string name, string date, string time, string destination, string origin, int first_class_wagons, int second_class_wagons);
+    ~TrainRide();
+    int reserve_any_seat(int class_type);
+};
+
+#endif // TRAINRIDE_H
