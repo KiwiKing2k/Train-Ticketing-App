886a15d3be38bb89a24e00e24771285de6de1a5f
Fireballer <dinustefan04@gmail.com>
2024-12-03 13:17:21 +0200

    Header file for Operator class file

---
+++ Admin/Operator.h
@@ -0,0 +1,32 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "TrainRide.h"
+
+using namespace std;
+
+typedef struct credentials
+{
+    string username;
+    string password;
+};
+
+class Operator
+{
+private:
+    string username;
+    string password;
+    bool authenticated = false;
+
+public:
+    Operator(string username, string password);
+    int login(string username, string password);
+    void add_ride();
+    void remove_ride();
+};
+
+#endif // OPERATOR_H
