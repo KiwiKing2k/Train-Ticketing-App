0ac3255dc7e6dad7d7b94d4e8ad82f2d4dc96f7a
Fireballer <dinustefan04@gmail.com>
2024-12-03 13:46:23 +0200

    Robust date check implemented

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -4,7 +4,7 @@
 #include <string>
 
 bool is_valid_date_format(const std::string& date);
-bool is_valid_year(const std::string& date);
+bool is_valid_time(const std::string& date);
 bool is_valid_city_name(const std::string& cityName);
 
 #endif // UTILS_H
